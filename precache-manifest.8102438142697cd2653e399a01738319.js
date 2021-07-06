618b1f5c89cafbcc3f77084679dea450db0a2887
DoHyunHwang <dohyun033@gmail.com>
2021-07-06 13:38:53 +0900

    Updates

---
+++ precache-manifest.8102438142697cd2653e399a01738319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4060f70693f1cb3e7e04c66aff651775",
+    "url": "/react_habit_tracker/index.html"
+  },
+  {
+    "revision": "47b4f9ae570690932e97",
+    "url": "/react_habit_tracker/static/css/main.6f007982.chunk.css"
+  },
+  {
+    "revision": "40b8165acbb243bdfb31",
+    "url": "/react_habit_tracker/static/js/2.2a4a2bc9.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/react_habit_tracker/static/js/2.2a4a2bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47b4f9ae570690932e97",
+    "url": "/react_habit_tracker/static/js/main.7c33bd64.chunk.js"
+  },
+  {
+    "revision": "a241dafdb097aafbf5f4",
+    "url": "/react_habit_tracker/static/js/runtime-main.2538eb4d.js"
+  }
+]);
